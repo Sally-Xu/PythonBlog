50d9683044698fcb6209cee2bbc57be484549dea
Sally Xu <sally.xu@bolstra.com>
2018-08-10 11:46:41 -0400

    change project name

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #bin/sh
-docker stop flaskdocker-ctn
-docker container rm flaskdocker-ctn
-docker image rm falskdocker-img 
-docker build -t flaskdocker-img .
-docker run -d -p 5000:5000 --name flaskdocker-ctn flaskdocker-img
-docker exec -it falskdocker bash
+docker stop pythonblog [containerid]
+docker container rm pythonblog
+docker image rm pythonblog 
+docker build -t pythonblog .
+docker run -d -p 5000:5000 --name pythonblog pythonblog
+docker exec -it pythonblog bash
